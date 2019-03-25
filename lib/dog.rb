d6cc91a6624d82a494b6d3bba64db06553b8975e
Ryan Pethel <arpethel@gmail.com>
2019-03-25 09:04:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,22 @@
 class Dog
-  #
-  # def initialize(first_name, last_name)
-  #   @first_name = first_name
-  #   @last_name = last_name
+  # def initialize(dogs_name)
+  #   @this_dogs_name = dogs_name
   # end
-
-  def initialize(dogs_name)
-    @this_dogs_name = dogs_name
-  end
 
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
-
+  
   def name
     @this_dogs_name
   end
 
-  def breed=(dogs_breed)
-    @this_dogs_breed = dogs_breed
-  end
-
-  def breed
-    @this_dogs_breed
-  end
+  # def breed=(dogs_breed)
+  #   @this_dogs_breed = dogs_breed
+  # end
+  # 
+  # def breed
+  #   @this_dogs_breed
+  # end
 
 end
